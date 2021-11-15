b73c1dbaeb2581a79055159cea9790b3301cb5dd
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2021-11-15 15:09:41 -0500

    Remove unnecessary parameters

--- src/passport-config.js
+++ src/passport-config.js
@@ -1,70 +1,62 @@
 //contains passport related info
-const LocalStrategy = require('passport-local')
-const bcrypt = require('bcrypt')
-
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
 
 //Mongo Setup
-const  ObjectId = require('mongodb').ObjectId;
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://smadsen:smadsen@userinformation.mgssl.mongodb.net/UserInformation?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://smadsen:smadsen@userinformation.mgssl.mongodb.net/UserInformation?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const dbName = "UserInformation";
 
+function initialize(passport) {
+  //function of authenticating users on login page. Done is a function that is called when we are done authenticating the user
+  const authenticateUser = async (email, password, done) => {
+    await client.connect();
 
-function initialize(passport, getUserByEmail,getUserById) {
+    const db = client.db(dbName);
+    const col = db.collection("user-info");
 
-    //function of authenticating users on login page. Done is a function that is called when we are done authenticating the user
-    const authenticateUser = async (email,password,done) => {
+    const user = await col.findOne({ email: email });
+    await client.close();
 
-        await client.connect();
-
-        const db = client.db(dbName);
-        const col = db.collection("user-info");
-
-        const user = await col.findOne({email:email})
-        await client.close();
-
-        if(user == null) {
-            return done(null,false, {message: 'No user found with that email'}) //false becuase no user found
-        }
-
-        try {
-            if(await bcrypt.compare(password,user.password)) {
-                return done(null, user)
-
-            }
-
-            else {
-                return done(null,false, {message: 'Password incorrect, Try again'})
-            }
-
-        }
-        catch (e) {
-            return done(e)
-        }
+    if (user == null) {
+      return done(null, false, { message: "No user found with that email" }); //false becuase no user found
     }
 
-    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Password incorrect, Try again" });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
 
-    //serilaize user to store inside of the session
-    passport.serializeUser((user,done) => done(null, user._id))
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
 
-    passport.deserializeUser(async (id, done) => {
-        await client.connect();
+  //serilaize user to store inside of the session
+  passport.serializeUser((user, done) => done(null, user._id));
 
-        const db = client.db(dbName);
-        const col = db.collection("user-info");
+  passport.deserializeUser(async (id, done) => {
+    await client.connect();
 
-         await col.findOne(
-          {_id: new ObjectId(id)},
-            (err, doc) => {
-              if(err){
-                return done(err);
-              }
-                return done(null, doc);
-            }
-        );
+    const db = client.db(dbName);
+    const col = db.collection("user-info");
+
+    await col.findOne({ _id: new ObjectId(id) }, (err, doc) => {
+      if (err) {
+        return done(err);
+      }
+      return done(null, doc);
     });
+  });
 }
 
-module.exports = initialize
+module.exports = initialize;
